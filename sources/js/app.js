4e8a29ab8325a975556850fee53300e329c79a02
Lukáš Pícha <lukas.picha@novy-web.cz>
2019-10-06 13:49:43 +0200

    refaktoring, napojeni skupinovych chatu z vue

--- sources/js/app.js
+++ sources/js/app.js
@@ -1,85 +1,144 @@
-$(document).ready(function(){
-	
-	var listOfUsers = [];
-	var url = 'https://private-d70ba-pwachat.apiary-mock.com/v1/users';
-	
-	$.ajax({
-	    url: url,
-	    type: 'get',
-	    async: false,
-	    dataType: "json",
-	    success: function(data) {
-	    	listOfUsers = data;
-	    }
-	});
+	$(document).ready(function(){
+		
+		var listOfUsers = listOfRooms = [];
+		var users_url = 'https://private-d70ba-pwachat.apiary-mock.com/v1/users';
 
-	var members = new Vue({
-		el: '#list-of-users',
-		data: {
-			users: listOfUsers
-		}
-	});
+		var rooms_url = 'https://private-d70ba-pwachat.apiary-mock.com/v1/rooms';
+		
+		
 
-
-	//test
-	var title = new Vue({
-		el: '.appname',
-		data: {
-			title: 'PWAChat'
-		}
-	});
-	var conversation = new Vue({
-			el: "#thread-panel",
+		var users_and_rooms = new Vue({
+			el: '#users-and-rooms',
 			data: {
-				conversation: null,
-				user_id: null
+				users: listOfUsers,
+				rooms: listOfRooms
 			},
 			methods: {
-				chatSide(userId) {
-					return userId == 1 ? "chat-left" : "chat-right";
-				},
-				convertDate(date) {
-					d = new Date(date);
-					return d.toLocaleString();
+				getAllAvatars(room) {
+					console.log(room);
+					var avatars = [];
+					$.each(room.users, function(index, user) {
+						avatars.push({
+        					"avatar": user.avatar,
+        					"name": user.name
+        				});
+    				});
+    				return avatars;
 				}
 			}
 		});
 
-	function loadThread(to) {
-		console.log(to);
-		var thread = [
-			{
-				"user": {
-					"id": 1,
-                	"name": "Martin Tejkl",
-                	"avatar": "https://www.bootdey.com/img/Content/avatar/avatar3.png",
-                	"status": "online"
-				},
-				"message": "<p>Hello bro, how are you?<br>Can we discuss our semestral project?</p>",
-				"created_at": "2014-03-12T13:37:27+00:00"
-			},
-			{
-				"user": {
-	                "id": 2,
-	                "name": "Pepa Reckziegel",
-	                "avatar": "https://www.bootdey.com/img/Content/avatar/avatar2.png",
-	                "status": "busy"
-            	},
-				"message": "<p>No<br>I'm little busy.</p>",
-				"created_at": "2014-03-12T13:37:27+00:00"
+		loadMyRooms();
+		loadUsers();
+
+
+
+
+
+
+
+		//test
+		var title = new Vue({
+			el: '.appname',
+			data: {
+				title: 'PWAChat'
 			}
-		];
-		conversation.user_id = 1;
-		conversation.conversation = thread;
-		conversation.oposite_id = to;
+		});
+		var conversation = new Vue({
+				el: "#thread-panel",
+				data: {
+					conversation: null,
+					user_id: null
+				},
+				methods: {
+					chatSide(userId) {
+						return userId == 1 ? "chat-left" : "chat-right";
+					},
+					convertDate(date) {
+						d = new Date(date);
+						return d.toLocaleString();
+					}
+				}
+			});
+
+		function loadThread(to) {
+			console.log(to);
+			var thread = [
+				{
+					"user": {
+						"id": 1,
+	                	"name": "Martin Tejkl",
+	                	"avatar": "https://www.bootdey.com/img/Content/avatar/avatar3.png",
+	                	"status": "online"
+					},
+					"message": "<p>Hello bro, how are you?<br>Can we discuss our semestral project?</p>",
+					"created_at": "2014-03-12T13:37:27+00:00"
+				},
+				{
+					"user": {
+		                "id": 2,
+		                "name": "Pepa Reckziegel",
+		                "avatar": "https://www.bootdey.com/img/Content/avatar/avatar2.png",
+		                "status": "busy"
+	            	},
+					"message": "<p>No<br>I'm little busy.</p>",
+					"created_at": "2014-03-12T13:37:27+00:00"
+				},
+				{
+					"user": {
+						"id": 1,
+	                	"name": "Martin Tejkl",
+	                	"avatar": "https://www.bootdey.com/img/Content/avatar/avatar3.png",
+	                	"status": "online"
+					},
+					"message": "<p>Hello bro, how are you?<br>Can we discuss our semestral project?</p>",
+					"created_at": "2014-03-12T13:37:27+00:00"
+				},
+				{
+					"user": {
+		                "id": 2,
+		                "name": "Pepa Reckziegel",
+		                "avatar": "https://www.bootdey.com/img/Content/avatar/avatar2.png",
+		                "status": "busy"
+	            	},
+					"message": "<p>No<br>I'm little busy.</p>",
+					"created_at": "2014-03-12T13:37:27+00:00"
+				}
+			];
+			conversation.user_id = 1;
+			conversation.conversation = thread;
+			conversation.oposite_id = to;
+
+		}
 
 
-		
+		$(".person").click(function() {
+			loadThread($(this).attr('data-chat'));
+		});
 
-	}
+		function loadMyRooms() {
+			$.ajax({
+			    url: rooms_url,
+			    type: 'get',
+			    async: false,
+			    dataType: "json",
+			    success: function(data) {
+			    	users_and_rooms.rooms = data;
+			    }
+			});
+		}
 
-
-	$(".person").click(function() {
-		loadThread($(this).attr('data-chat'));
+		function loadUsers() {
+			$.ajax({
+			    url: users_url,
+			    type: 'get',
+			    async: false,
+			    dataType: "json",
+			    success: function(data) {
+			    	users_and_rooms.users = data;
+			    }
+			});
+		}
 	});
-});
+
+	
